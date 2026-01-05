02a4b17140b461961f7009afbd26225f84616ad8
Karonzer <knkhsu122@naver.com>
2026-01-05 19:43:43 +0900

    작업 업로드

--- Program/Assets/Class13th/Unit.cs
+++ Program/Assets/Class13th/Unit.cs
@@ -1,18 +1,47 @@
+using System;
 using UnityEngine;
+
+public enum AniType
+{
+    Idle,
+    Die,
+    Attack,
+    Walk
+}
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
     [SerializeField] private AudioSource audioSource;
-    [SerializeField] private AudioClip clip;
+
+    [SerializeField] private AniType index;
 
     private void Awake()
     {
+        animator = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();    
+    }
+
+    private void OnEnable()
+    {
+        index = 0;
+    }
+
+    private void Update()
+    {
+
+    }
+
+
+
+    public void Transtion(AniType _count)
+    {
+        index = _count;
     }
 
     public void Emit()
     {
-        AudioClip loadClip = Resources.Load<AudioClip>("Attack");
+        AudioClip loadClip = Resources.Load<AudioClip>(index.ToString());
         audioSource.PlayOneShot(loadClip);
         Debug.Log($"audioSource.PlayOneShot(clip) : {loadClip.name}");
     }
